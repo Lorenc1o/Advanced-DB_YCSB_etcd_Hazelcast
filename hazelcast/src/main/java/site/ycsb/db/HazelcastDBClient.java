aa1f2c194f58d3c677b91852243905060ce48e76
Liliia Aliakberova <liliiaaliakberova@Liliias-MacBook-Pro.local>
2022-11-22 17:53:55 +0100

    StringByterIterator used to ensure conversion from ByteIterator to String

--- hazelcast/src/main/java/site/ycsb/db/HazelcastDBClient.java
+++ hazelcast/src/main/java/site/ycsb/db/HazelcastDBClient.java
@@ -1,4 +1,4 @@
-package site.ycsb.db;
+package site.ycsb.db.hazelcast;
 
 import site.ycsb.DB;
 import site.ycsb.DBException;
@@ -6,18 +6,14 @@ import site.ycsb.ByteIterator;
 import site.ycsb.StringByteIterator;
 import site.ycsb.Status;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutionException;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 /**
  * YCSB binding for <a href="https://hazelcast.com/">hazelcast</a>.
@@ -28,67 +24,52 @@ public class HazelcastDBClient extends DB{
   private static HazelcastInstance hz;
   private boolean async = false;
 
-  @Override
   public void init() throws DBException{
     hz = HazelcastClient.newHazelcastClient();
   }
 
   @Override
-  public Status int read(String table, String key, Set<String> fields, HashMap<String, ByteIterator> result) {
+  public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+    IMap<String, HashMap<String, String>> map = hz.getMap(table);
+    Map<String, String> resultMap = map.get(key);
+    StringByteIterator.putAllAsByteIterators(result, resultMap);
+    return Status.OK;
+  }
+  @Override
+  public Status insert(String table, String key, Map<String, ByteIterator> values){
+    Map<String, String> strValues = StringByteIterator.getStringMap(values);
+    IMap<String, Map<String, String>> distributedMap = hz.getMap(table);
+    Future<Map<String, String>> future = (Future<Map<String, String>>) distributedMap.putAsync(key, strValues);
+    return Status.OK;
+  }
+  @Override
+  public Status delete(String table, String key) {
     ConcurrentMap<String, Map<String, String>> distributedMap = hz.getMap(table);
-    Map<String, ByteIterator> resultMap = distributedMap.get(key);
-    result.putAll(resultMap);
-    if(result.isEmpty()) {
-      return Status.ERROR;
+    distributedMap.remove(key);
+    return Status.OK;
+  }
+  
+  @Override
+  public Status update(String table, String key, Map<String, ByteIterator> values){
+    IMap<String, Map<String, String>> distributedMap = hz.getMap(table);
+    if (values != null && values.size() > 0) {
+      Map<String, String> resultMap = distributedMap.get(key);
+      StringByteIterator.putAllAsStrings(resultMap, values);
+      Future<Map<String, String>> future = (Future<Map<String, String>>) distributedMap.putAsync(key, resultMap);
     }
     return Status.OK;
   }
 
-  public static int insert(String table, String key, HashMap<String, String> values)
-  throws InterruptedException, ExecutionException, TimeoutException {
-    IMap<String, Map<String, String>> distributedMap = hz.getMap(table);
-    Future<Map<String, String>> future = (Future<Map<String, String>>) distributedMap.putAsync(key, values);
-    future.get(50, TimeUnit.MINUTES);
-    return 1;
-  }
-
-  public static int delete(String table, String key) {
-    ConcurrentMap<String, Map<String, String>> distributedMap = hz.getMap(table);
-    distributedMap.remove(key);
-    return 1;
-  }
-
-  public int update(String table, String key, HashMap<String, String> values)
-          throws InterruptedException, ExecutionException, TimeoutException {
-    IMap<String, Map<String, String>> distributedMap = hz.getMap(table);
-    if (values != null && values.size() > 0) {
-      Map<String, String> resultMap = distributedMap.get(key);
-      Iterator<String> iter = values.keySet().iterator();
-      String k = null;
-      while (iter.hasNext()) {
-        k = iter.next();
-        resultMap.put(k, values.get(k));
-      }
-      if (this.async) {
-        Future<Map<String, String>> future = (Future<Map<String, String>>)
-            distributedMap.putAsync(key, values);
-        future.get(50, TimeUnit.MINUTES);
-      } else {
-        distributedMap.put(key, resultMap);
-      }
-    }
-    return 1;
-  }
-
-  public static int scan(String table, String startkey, int recordcount,
-      Set<String> fields, Vector<HashMap<String, String>> result) {
+  @Override
+  public Status scan(String table, String startkey, int recordcount,
+      Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
     String key;
     for (int i = 0; i < recordcount; i++) {
       key = startkey + i;
-      if (read(table, key, fields, result.get(i)) == 0) {
-        return 0;
+      if (read(table, key, fields, result.get(i)) == Status.ERROR) {
+        return Status.ERROR;
       }
     }
-    return 1;
+    return Status.OK;
   }
 }
